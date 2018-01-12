760ac72c98ac70d57b1c7e09ac95f7fced0b07f7
Matt.Wilkie <Matt.Wilkie@gov.yk.ca>
2018-01-11 16:00:57 -0800

    successfully reads 1 ini file and reports results
            \\ENVGEOSERVER\Maps\Index
                           title: Map Index
                           owner: Matt Wilkie
                          client: Geomatics Unit
                          branch: IMT
                  date_initiated: 2009-02-26

--- xx-tester.py
+++ xx-tester.py
@@ -7,11 +7,17 @@ dirFile = os.path.join(baseDir, 'Map-Project-Index.html')
 metafile = 'project.ini'
 
 folders = ['Index']
+section = 'project'
+
 for d in folders:
     location = os.path.join(baseDir, d)
     print(location)
     config.read(os.path.join(location, metafile))
-    if 'project' in config.sections():
-        for key in config['project']: print(key)
+    if section in config.sections():
+        fields = config.options(section)
+        for f in fields:
+            v = config.get(section, f)
+            print('{:>20}: {}'.format(f, v))
+            
     
 
