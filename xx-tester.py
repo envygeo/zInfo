6289d841130f64b6fb6de152b25a50f914d7c328
Matt.Wilkie <Matt.Wilkie@gov.yk.ca>
2019-05-07 16:33:13 -0700

    start building testing harness inside the code repo

--- xx-tester.py
+++ xx-tester.py
@@ -1,23 +1,42 @@
 import os
+import glob
+import inspect
 import configparser
 config = configparser.ConfigParser()
 
-baseDir = r'\\ENVGEOSERVER\Maps'
-dirFile = os.path.join(baseDir, 'Map-Project-Index.html')
+def get_script_dir():
+    """Return folder of this running script's location"""
+    # thanks @Jose, https://stackoverflow.com/a/44592299/14420
+    ourscript = inspect.getframeinfo(inspect.currentframe()).filename
+    dir = os.path.dirname(os.path.abspath(ourscript))
+    return dir
+
+baseDir = os.path.join(get_script_dir(), 'test/in')
+
+dirFile = os.path.join(baseDir, 'test/out', 'Map-Project-Index.html')
 metafile = 'project.ini'
 
-folders = ['Index']
-section = 'project'
+def find_metafiles(target, ini):
+    '''Return list of project.ini files under target'''
+    return filename in glob.glob(os.path.join(target, ini) , recursive=True)
 
-for d in folders:
-    location = os.path.join(baseDir, d)
-    print(location)
-    config.read(os.path.join(location, metafile))
-    if section in config.sections():
-        fields = config.options(section)
-        for f in fields:
-            v = config.get(section, f)
-            print('{:>20}: {}'.format(f, v))
-            
-    
+
+def main():
+    folders = ['Index']
+    section = 'project'
+    for d in folders:
+        location = os.path.join(baseDir, d)
+        print(location)
+        config.read(os.path.join(location, metafile))
+        if section in config.sections():
+            fields = config.options(section)
+            for f in fields:
+                v = config.get(section, f)
+                print('{:>20}: {}'.format(f, v))
+
+
+if __name__ == "__main__":
+    #ROOT = get_script_dir()
+    #print(ROOT)
+    main()
 
