b6e09c809e0940cc5377af2eac593a22df109f76
Matt.Wilkie <Matt.Wilkie@gov.yk.ca>
2019-05-23 08:51:19 -0700

    now creates out html file, w/ project metadata and clickable link to home folder

--- xx-tester.py
+++ xx-tester.py
@@ -6,7 +6,6 @@ config = configparser.ConfigParser()
 
 INI = 'project.ini' # name of metadata file indicating a project folder
 
-
 def get_script_dir():
     """Return folder of this running script's location"""
     # thanks @Jose, https://stackoverflow.com/a/44592299/14420
@@ -15,8 +14,11 @@ def get_script_dir():
     return dir
 
 baseDir = os.path.join(get_script_dir(), 'test\\in')
+outDir = os.path.join(get_script_dir(), 'test\\out')
+if not os.path.exists(outDir):
+    os.makedirs(outDir)
 
-dirFile = os.path.join(baseDir, 'test\\out', 'Map-Project-Index.html')
+INDEX = os.path.join(outDir, 'Map-Project-Index.html')
 
 
 def find_metafiles(target, ini):
@@ -48,6 +50,15 @@ def write(data, fname):
 
 
 htmlheader = """<HTML>
+<style>
+    dt {
+    display: block;
+    float: left;
+    min-width: 6em;
+    margin-left: 2em;
+    font-weight: bold;
+    }
+</style>
 <body>
     <h1>ENV Geomatics Maps & Projects</h1>
     <p>&nbsp;</p>
@@ -66,10 +77,17 @@ htmlfooter = """
 </HTML>"""
 
 def dict2html(dictionary):
+    '''parse project dictionary into html definition list'''
     for d in dictionary:
-        html = f'<h2>{d}</h2>'
+        # the project home folder
+        dir = os.path.dirname(f'{d}')
+        html = f'<h2><a href="file:///{dir}">{dir}</a></h2>'
+        # the project particulars
         for i in dictionary[d]:
-            html += f'<li>{i}</li>'
+            for k,v in i.items():
+                html += '<dd>'
+                html += f'<dt>{k}</dt><dd>{v}</dd>'
+                html += '</dd>'
     return html
 
 
@@ -79,7 +97,8 @@ if __name__ == "__main__":
     #print(metafiles)
     projects_d = {}
     projects_d[metafiles[0]] = [parse_ini(f) for f in metafiles]
-    html = dict2html(projects_d)
+    html = htmlheader + dict2html(projects_d) + htmlfooter
+    write(html, INDEX)
 
 
 
